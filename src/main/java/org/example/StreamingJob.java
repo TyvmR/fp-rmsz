3f534772c41e63a1351763da038408067f0bcbab
john.huang <john.huang@cloudwise.com>
2021-12-10 10:34:30 +0800

    DataStreamApi的常用方法

--- src/main/java/org/example/StreamingJob.java
+++ src/main/java/org/example/StreamingJob.java
@@ -1,24 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package org.example;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.Partitioner;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple0;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.example.udsource.MyDataSource;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -36,29 +31,79 @@ public class StreamingJob {
 
 	public static void main(String[] args) throws Exception {
 		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * https://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
-
-		// execute program
+		 StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		 env.setParallelism(5);
+		 DataStreamSource<Long>     longDataStreamSource = env.addSource(new MyParDataSource());
+		 SingleOutputStreamOperator<Tuple1<Long>> map = longDataStreamSource.map(new MapFunction<Long, Tuple1<Long>>() {
+			@Override
+			public Tuple1<Long> map(Long value) throws Exception {
+				return new Tuple1<>(value);
+			}
+		});
+		 DataStream<Tuple1<Long>> tuple1DataStream = map.partitionCustom(new CustomPartition(), 0);
+		 SingleOutputStreamOperator<Long> result = tuple1DataStream.map(new MapFunction<Tuple1<Long>, Long>() {
+			@Override
+			public Long map(Tuple1<Long> value) throws Exception {
+				System.out.println("线程ID为:" + Thread.currentThread().getId() + ", value:" + value);
+				return value.getField(0);
+			}
+		});
+		result.print().setParallelism(2);
 		env.execute("Flink Streaming Java API Skeleton");
+	}
+
+
+
+   public static class MyDataSource implements SourceFunction<Long> {
+
+
+		private long count  = 0;
+
+		@Override
+		public void run(SourceContext<Long> sourceContext) throws Exception {
+			while (true){
+				sourceContext.collect(count);
+				count++;
+				Thread.sleep(1000L);
+			}
+		}
+
+		@Override
+		public void cancel() {
+
+		}
+	}
+
+	public static class MyParDataSource implements ParallelSourceFunction<Long> {
+
+
+		private long count  = 0;
+
+		@Override
+		public void run(SourceContext<Long> sourceContext) throws Exception {
+			while (true){
+				sourceContext.collect(count);
+				count++;
+				Thread.sleep(1000L);
+			}
+		}
+
+		@Override
+		public void cancel() {
+
+		}
+	}
+
+
+	public static class CustomPartition implements Partitioner<Long>{
+
+		@Override
+		public int partition(Long aLong, int i) {
+			if(aLong % 2 == 0){
+				return 0;
+			}else {
+				return 1;
+			}
+		}
 	}
 }
---
+++ src/main/java/org/example/udsource/MyDataSource.java
@@ -0,0 +1,33 @@
+package org.example.udsource;
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+/**
+ * Copyright (C), 2021-2021
+ * FileName: MyDataSource
+ * Author:   Administrator
+ * Date:     2021/12/10 9:51
+ * Description:
+ */
+
+public class MyDataSource implements SourceFunction<Long> {
+
+
+    private long count  = 0;
+
+    @Override
+    public void run(SourceContext<Long> sourceContext) throws Exception {
+            while (true){
+                sourceContext.collect(count);
+                count++;
+                Thread.sleep(1L);
+            }
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
+
+
