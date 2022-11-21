6e93601a2665a83ab923f55ed2a7c02f887d9c01
john.huang <john.huang@cloudwise.com>
2022-11-21 13:47:00 +0800

    一个mysqlcdc的demo

--- src/main/java/org/example/exp1/CDCDemoV1.java
+++ src/main/java/org/example/exp1/CDCDemoV1.java
@@ -1,6 +1,8 @@
 package org.example.exp1;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 /**
@@ -16,6 +18,9 @@ public class CDCDemoV1 {
         executionEnvironment.setParallelism(1);
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(executionEnvironment);
         String sourceTable = "CREATE TABLE mysql_table (\n" +
+                "    db_name STRING METADATA FROM 'database_name' VIRTUAL,\n" +
+            "        table_name STRING METADATA  FROM 'table_name' VIRTUAL,\n" +
+                "    operation_ts TIMESTAMP_LTZ(3) METADATA FROM 'op_ts' VIRTUAL,\n" +
                 "     id INT,\n" +
                 "\t name STRING,\n" +
                 "     op_time INT,\n" +
@@ -31,22 +36,27 @@ public class CDCDemoV1 {
                 "     'table-name' = 'newtable')\n" +
                 "  ";
 
-        String sinkTable = "CREATE TABLE kafka_table (\n" +
-                "  `id` INT,\n" +
-                "  `name` STRING,\n" +
-                "  `op_time` INT,\n" +
-                "  `age` INT,\n" +
-                "  PRIMARY KEY (id) NOT ENFORCED " +
-                ") WITH (\n" +
-                "  'connector' = 'upsert-kafka',\n" +
-                "  'topic' = 'john_test',\n" +
-                "  'properties.bootstrap.servers' = '10.0.12.240:18108,10.0.12.252:18108,10.0.12.253:18108',\n" +
-                "  'properties.group.id' = 'testGroup12',\n" +
-                "  'key.format' = 'json',\n" +
-                "  'value.format' = 'json' " +
-                ")";
+//        String sinkTable = "CREATE TABLE kafka_table (\n" +
+//                "    `db_name` STRING ,\n" +
+//            "        `table_name` STRING,\n" +
+//                "    `operation_ts` TIMESTAMP(3) ,\n" +
+//                "  `id` INT,\n" +
+//                "  `name` STRING,\n" +
+//                "  `op_time` INT,\n" +
+//                "  `age` INT,\n" +
+//                "  PRIMARY KEY (id) NOT ENFORCED " +
+//                ") WITH (\n" +
+//                "  'connector' = 'upsert-kafka',\n" +
+//                "  'topic' = 'john_test',\n" +
+//                "  'properties.bootstrap.servers' = '10.0.12.240:18108,10.0.12.252:18108,10.0.12.253:18108',\n" +
+//                "  'properties.group.id' = 'testGroup12',\n" +
+//                "  'key.format' = 'json',\n" +
+//                "  'value.format' = 'json' " +
+//                ")";
         tEnv.executeSql(sourceTable);
-        tEnv.executeSql(sinkTable);
-        tEnv.executeSql(" insert into kafka_table select * from  mysql_table");
+//        tEnv.executeSql(sinkTable);
+//        tEnv.executeSql(" insert into kafka_table select * from  mysql_table");
+        TableResult tableResult = tEnv.executeSql(" select * from  mysql_table");
+        tableResult.print();
     }
 }
